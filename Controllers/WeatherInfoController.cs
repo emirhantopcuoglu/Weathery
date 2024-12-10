649e8b9376dd400634a77cc8798c496a1c0c5340
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-12-10 14:33:22 +0300

    Frontend gelistirmeleri yapildi.

--- Controllers/WeatherInfoController.cs
+++ Controllers/WeatherInfoController.cs
@@ -15,7 +15,19 @@ namespace Weathery.Controllers
 
         public async Task<IActionResult> Index(string city = "Istanbul")
         {
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                return RedirectToAction("Index", "WeatherInfo");
+            }
+
             var weatherInfo = await GetWeatherInfo(city);
+
+            if (weatherInfo == null)
+            {
+                TempData["Error"] = "Şehir ismi hatalı. Lütfen tekrar deneyiniz.";
+                return RedirectToAction("Index", new { city = "Istanbul" });
+            }
+
             return View(weatherInfo);
         }
 
@@ -25,20 +37,58 @@ namespace Weathery.Controllers
 
             using (var client = new HttpClient())
             {
-                string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
-
-                var response = await client.GetStringAsync(url);
-
-                var data = JsonSerializer.Deserialize<JsonElement>(response);
-
-                return new WeatherInfo
+                string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang=tr&units=metric";
+                try
                 {
-                    City = data.GetProperty("name").GetString(),
-                    Temperature = data.GetProperty("main").GetProperty("temp").GetDouble(),
-                    Description = data.GetProperty("weather")[0].GetProperty("description").GetString(),
-                    Humidity = data.GetProperty("main").GetProperty("humidity").GetDouble()
-                };
+                    var response = await client.GetAsync(url);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return null;
+                    }
+                    var responseData = await response.Content.ReadAsStringAsync();
+
+                    var data = JsonSerializer.Deserialize<JsonElement>(responseData);
+
+                    var sunriseUnix = data.GetProperty("sys").GetProperty("sunrise").GetDouble();
+
+                    var sunsetUnix = data.GetProperty("sys").GetProperty("sunset").GetDouble();
+
+                    return new WeatherInfo
+                    {
+                        City = data.GetProperty("name").GetString(),
+
+                        Icon = data.GetProperty("weather")[0].GetProperty("icon").GetString(),
+
+                        Temperature = data.GetProperty("main").GetProperty("temp").GetDouble(),
+
+                        Description = data.GetProperty("weather")[0].GetProperty("description").GetString(),
+
+                        Humidity = data.GetProperty("main").GetProperty("humidity").GetDouble(),
+
+                        WindSpeed = data.GetProperty("wind").GetProperty("speed").GetDouble(),
+
+                        Rain = data.TryGetProperty("rain", out var rainData) ? rainData.GetProperty("1h").GetDouble() : 0.0,
+
+                        SunRise = UnixTimeToDateTime(sunriseUnix),
+
+                        SunSet = UnixTimeToDateTime(sunsetUnix)
+                    };
+                }
+                catch (Exception ex)
+                {
+
+                    return null;
+                }
+
+
+
+
             }
+        }
+        public static DateTime UnixTimeToDateTime(double unixTime)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds((long)unixTime);
+            return dateTimeOffset.DateTime.ToLocalTime(); // Yerel saat dilimine göre dönüştürme
         }
 
     }
--- Models/WeatherInfo.cs
+++ Models/WeatherInfo.cs
@@ -3,8 +3,16 @@ namespace Weathery.Models
     public class WeatherInfo
     {
         public string? City { get; set; }
+        public string? Icon { get; set; }
         public double Temperature { get; set; }
         public string? Description { get; set; }
         public double Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public double Rain { get; set; }
+        public DateTime SunRise { get; set; }
+        public DateTime SunSet { get; set; }
+
     }
+
+    
 }
